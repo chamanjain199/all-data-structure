d6ca894bc689c813789a3dd5a3baf18b0fc82d08
Chaman jain <31966120+chamanjain199@users.noreply.github.com>
2021-12-28 22:59:53 +0530

    SSL

--- src/alldatastructure/linked/list/Node.java
+++ src/alldatastructure/linked/list/Node.java
@@ -3,7 +3,13 @@ package alldatastructure.linked.list;
 public class Node {
 	public int value;
 	public Node next;
-	
-	
+
+	@Override
+	public String toString() {
+		if (next != null)
+			return value + "," + next;
+		else
+			return value+"";
+	}
 
 }
--- src/alldatastructure/linked/list/SinglyLinkedList.java
+++ src/alldatastructure/linked/list/SinglyLinkedList.java
@@ -1,29 +1,103 @@
 package alldatastructure.linked.list;
 
+import java.util.Iterator;
+
 public class SinglyLinkedList {
 
 	public Node head;
 	public Node tail;
 	public int size;
 
-	public Node add(int value) {
+	public Node add(int value, int index) {
 
 		Node newNode = new Node();
 		newNode.value = value;
 
-		if (size == 0) {
+		if (index == 0 && size == 0) {
 			head = newNode;
 			tail = newNode;
-		} else {
 
+		} else if (index == 0) {
+			newNode.next = head;
+			head = newNode;
+		} else if (index > size) {
 			tail.next = newNode;
 			tail = newNode;
-
+		} else if (index <= size) {
+			Node temp = head;
+			while (index > 1) {
+				temp = temp.next;
+				index--;
+			}
+			newNode.next = temp.next;
+			temp.next = newNode;
 		}
 
 		size++;
 
 		return head;
+	}
+
+	public void traverse() {
+		Node node = head;
+		System.out.print("[");
+
+		for (int i = 0; i < size; i++) {
+			System.out.print(node.value);
+
+			node = node.next;
+			if (i != size - 1)
+				System.out.print(",");
+		}
+
+		System.out.print("]");
+
+	}
+
+	boolean searchNode(int value) {
+
+		Node node = head;
+		while (node != null && node.value != value) {
+			node = node.next;
+		}
+
+		return node != null;
+	}
+
+	public void deletionOfNode(int location) {
+
+		if (head == null) {
+			System.out.println("No Element in SLL");
+		}
+		if (location == 0) {
+			if (size == 1) {
+				head = null;
+				tail = null;
+			} else {
+				head = head.next;
+			}
+			size--;
+		} else {
+			Node node = head;
+			for (int i = 0; i < location - 2; i++) {
+				node = node.next;
+			}
+			if (location == size) {
+				node.next=null;
+				tail = node;
+			}else {
+				Node delNode=node.next;
+				Node nextNode=delNode.next;
+				node.next=nextNode;
+			}
+			size--;
+		}
+
+	}
+
+	@Override
+	public String toString() {
+		return "SinglyLinkedList [" + head + "]";
 	}
 
 }
